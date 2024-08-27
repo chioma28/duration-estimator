d4c3cf9f1effc61feaacc865eb6e1a571cac0dd7
Chioma Okeke <co22aat@herts.ac.uk>
2024-08-27 06:01:24 +0100

    modified edit duration

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def estimate_duration(title):
     similar_task_duration = data['duration'].iloc[most_similar_index]
     similar_task_title = data['title'].iloc[most_similar_index]
 
-    print("The most similar task is ", similar_task_title, "with a duration of ",  {similar_task_duration}, "minutes ")
+    # print("The most similar task is ", similar_task_title, "with a duration of ",  {similar_task_duration}, "minutes ")
 
 
     new_task_features = pd.DataFrame({
